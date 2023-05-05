6f86217adffaa47eca31338873ccdeab1001893f
madrimovDev <madrimov5014@gmail.com>
2023-05-05 14:59:20 +0500

    groups table

--- src/components/groups/GroupsTable.tsx
+++ src/components/groups/GroupsTable.tsx
@@ -1,5 +1,76 @@
-import {Table} from 'antd';
+import {getPathItem} from '@/layouts/lib/getPathItem';
+import {useAppSelector} from '@/store/hooks/useAppSelector';
+import {DeleteFilled, EditFilled} from '@ant-design/icons';
+import {Button, Space, Table} from 'antd';
+import {Link, useLocation} from 'react-router-dom';
 
 export default function GroupsTable(): JSX.Element {
-  return <Table bordered pagination={false} dataSource={[]}/>;
+  const {loading, group} = useAppSelector((state) => state.groups);
+  const {pathname} = useLocation();
+  const path = getPathItem(pathname, 1);
+  return (
+    <Table
+      bordered
+      loading={loading}
+      pagination={false}
+      dataSource={group ?? []}
+      columns={[
+        {
+          key: 'index',
+          title: '№',
+          render(_, _record, index) {
+            return index + 1;
+          },
+        },
+        {
+          key: 'Name',
+          title: 'Name',
+          render(_, record) {
+            return <Link to={record.id.toString()}>{record.name}</Link>;
+          },
+        },
+        {
+          key: 'direction',
+          title: 'Direction',
+          render(_, record) {
+            return (
+              <Link to={`/${path}/directions/${record.direction.id}`}>
+                {record.direction.name}
+              </Link>
+            );
+          },
+        },
+        {
+          key: 'month',
+          title: 'Month',
+          render(_, record) {
+            return record.months;
+          },
+        },
+        {
+          key: 'Teacher',
+          title: 'Teacher',
+          render(_, record) {
+            return (
+              <Link to={`/${path}/teachers/${record.teacher?.id ?? ''}`}>
+                {record.teacher?.name} {record.teacher?.surname}
+              </Link>
+            );
+          },
+        },
+        {
+          key: 'actions',
+          title: '',
+          render() {
+            return (
+              <Space>
+                <Button icon={<EditFilled />} />
+                <Button danger icon={<DeleteFilled />} />
+              </Space>
+            );
+          },
+        },
+      ]}
+    />
+  );
 }
--- src/pages/groups/Groups.tsx
+++ src/pages/groups/Groups.tsx
@@ -1,14 +1,19 @@
 import {GroupsTable} from '@/components/groups';
+import {useAppDispatch} from '@/store/hooks/useAppDispatch';
+import {getAllGroups} from '@/store/reducers/groups';
 import {Button, Col, Row, Space} from 'antd';
+import {useEffect} from 'react';
 
 export default function Groups(): JSX.Element {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    void dispatch(getAllGroups());
+  }, []);
   return (
     <Row className='p-4'>
       <Col offset={4} span={16}>
         <Space className='justify-end w-full my-10'>
-          <Button type='primary'>
-            Create Group
-          </Button>
+          <Button type='primary'>Create Group</Button>
         </Space>
         <GroupsTable />
       </Col>
