4cde29f2065ccbdc0bee93a0b7d550d6364eada7
madrimovDev <madrimov5014@gmail.com>
2023-06-01 11:22:27 +0500

    modified logo

--- src/layouts/rootLayout/RootSider.tsx
+++ src/layouts/rootLayout/RootSider.tsx
@@ -6,7 +6,7 @@ export default function RootSider(props: PropsWithChildren): JSX.Element {
     <Layout.Sider className='!fixed !overflow-auto !left-0 !top-0 !bottom-0'>
       <div className='px-4 py-3'>
         <Typography.Title
-          className='flex justify-center active:bg-blue-500 active:scale-75 transition-all bg-gray-500/30 py-2 rounded-md !mb-0 !text-2xl !text-white'
+          className='flex select-none cursor-pointer touch-none justify-center active:shadow-[inset_0_0_30px_,_0_0_30px] border-2 border-transparent active:border-blue-300/80 active:shadow-blue-500 active:scale-90 transition-all bg-gray-500/30 py-2 rounded-md !mb-0 !text-2xl !text-white'
           level={5}
         >
           <span className='mr-2'>DATA</span>
